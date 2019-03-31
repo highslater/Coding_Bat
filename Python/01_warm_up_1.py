b3595395e30b54028c08d9d8e6f3cc4dbc3567a2
highslater <highslater@hotmail.com>
2019-03-31 13:28:04 -0400

    commit

--- Python/01_warm_up_1.py
+++ Python/01_warm_up_1.py
@@ -105,7 +105,6 @@ def makes10(a, b):
 '''
 Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
 
-
 makes10(9, 10) → True
 makes10(9, 9) → False
 makes10(1, 9) → True
@@ -113,3 +112,121 @@ makes10(1, 9) → True
 # print(makes10(9, 10))
 # print(makes10(9, 9))
 # print(makes10(1, 9))
+
+
+# 7
+def near_hundred(n):
+    """Warmup-1 > near_hundred."""
+    return abs(100 - n) <= 10 or abs(200 - n) <= 10
+
+'''
+Given an int n, return True if it is within 10 of 100 or 200.
+Note: abs(num) computes the absolute value of a number.
+
+near_hundred(93) → True
+near_hundred(90) → True
+near_hundred(89) → False
+'''
+# print(near_hundred(93))
+# print(near_hundred(90))
+# print(near_hundred(89))
+
+
+# 8
+def pos_neg(a, b, negative):
+    """Warmup-1 > pos_neg."""
+    return [a < 0 and b > 0 or a > 0 and b < 0,
+            a < 0 and b < 0][negative]
+
+'''
+Given 2 int values, return True if one is negative and one is positive.
+Except if the parameter "negative" is True,
+then return True only if both are negative.
+
+pos_neg(1, -1, False) → True
+pos_neg(-1, 1, False) → True
+pos_neg(-4, -5, True) → True
+'''
+# print(pos_neg(1, -1, False))
+# print(pos_neg(-1, 1, False))
+# print(pos_neg(-4, -5, True))
+
+
+# 9
+def not_string(str):
+    """Warmup-1 > not_str."""
+    return ['not ' + str, str][str.startswith('not')]
+
+'''
+Given a string, return a new string where "not " has been added to the front.
+However, if the string already begins with "not", return the string unchanged.
+
+not_string('candy') → 'not candy'
+not_string('x') → 'not x'
+not_string('not bad') → 'not bad'
+'''
+# print(not_string('candy'))
+# print(not_string('x'))
+# print(not_string('not bad'))
+
+
+# 10
+def missing_char(str, n):
+    """Warmup-1 > missing_char."""
+    return str[:n] + str[n + 1:]
+
+'''
+Given a non-empty string and an int n, return a new string where the char at
+index n has been removed. The value of n will be a valid index of a char in
+the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+'''
+
+
+# 11
+def front_back(str):
+    """Warmup-1 > front_back."""
+    if len(str) >= 2:
+        f = str[0]
+        b = str[-1]
+        m = str[1:-1]
+        return b + m + f
+    else:
+        return str
+
+'''
+Given a string, return a new string where the first and last chars have
+been exchanged.
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+'''
+# print(front_back('code'))
+# print(front_back('a'))
+# print(front_back('ab'))
+
+
+# 12
+def front3(str):
+    """Warmup-1 > front3."""
+    if len(str) >= 3:
+        return (str[0:3]) * 3
+    else:
+        return str * 3
+
+'''
+Given a string, we'll say that the front is the first 3 chars of the string.
+If the string length is less than 3, the front is whatever is there.
+Return a new string which is 3 copies of the front.
+
+front3('Java') → 'JavJavJav'
+front3('Chocolate') → 'ChoChoCho'
+front3('abc') → 'abcabcabc'
+'''
+# print(front3('Java'))
+# print(front3('Chocolate'))
+# print(front3('abc'))
