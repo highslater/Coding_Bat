3d9939af38f95fe0819c85c22d4b8e4a9326e5e0
highslater <highslater@hotmail.com>
2019-04-02 01:06:18 -0400

    commit

---
+++ Python/02_warm_up_2.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.7
+
+"""Docstring."""
+
+
+# 1
+def string_times(str, n):
+    """Warmup-2 > string_times."""
+    return str * n
+
+'''
+Given a string and a non-negative int n, return a larger string that is
+n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+'''
+# print(string_times('Hi', 2))
+# print(string_times('Hi', 3))
+# print(string_times('Hi', 1))
